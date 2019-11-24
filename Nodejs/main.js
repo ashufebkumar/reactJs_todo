83cbc3effff256dd84d4ef79b0ab083511020bc2
Ashutosh Kumar <ashufeb.kumar@gmail.com>
2019-11-25 00:54:36 +0530

    small changesf

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -62,12 +62,12 @@ var eventEmitter = new events.EventEmitter();
 
 // listener #1
 var listner1 = function listner1() {
-   console.log('listner1 executed.');
+   console.log('listner1 executed.See carefully');
 }
 
 // listener #2
 var listner2 = function listner2() {
-   console.log('listner2 executed.');
+   console.log('listner2 executed. Let\'s check what happen\'s ');
 }
 
 // Bind the connection event with the listner1 function
